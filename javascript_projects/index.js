2b7adc539604a6bedb694084b906d81ca7fae6fa
abhishekkhanna2710 <abhishekkhanna2710@gmail.com>
2023-03-10 17:55:56 +0530

    added loading

--- javascript_projects/index.js
+++ javascript_projects/index.js
@@ -1,18 +1,35 @@
-function getData() {
-    const url = "https://jsonplaceholder.typicode.com/todos";
+async function getData() {
+    const url = "https://jsonplaceholder.typicode.com/posts";
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data =>
-            // console.log(data[0])
-            displayData(data)
-        )
-        .catch(err => {
-            console.log(err)
-        })
+    // fetch(url)
+    //     .then(res => res.json())
+    //     .then(data =>
+    //         // console.log(data[0])
+    //         displayData(data)
+    //     )
+    //     .catch(err => {
+    //         console.log(err)
+    //     })
+
+
+    // await async
+
+    const response = await fetch(url);
+
+    const data = await response.json();
+    console.log(data)
+    if (response) {
+        hideloader();
+    }
+    displayData(data);
+
 }
 
 getData();
+
+function hideloader() {
+    document.getElementById('loading').style.display = 'none'
+}
 
 
 function displayData(data) {
@@ -22,7 +39,7 @@ function displayData(data) {
 
     data.forEach(e => {
         let li = document.createElement('li');
-        li.innerHTML = `${e.id} - ${e.title} - ${e.completed}`;
+        li.innerHTML = `${e.id} - ${e.title} - ${e.body}`;
         ul.appendChild(li);
 
     })
