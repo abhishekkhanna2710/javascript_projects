0fa793cbdfa1dcf29b1b9d88cabd1af936fd822e
abhishekkhanna2710 <abhishekkhanna2710@gmail.com>
2023-03-12 15:00:29 +0530

    closures output

---
+++ javascript_projects/closures/index.js
@@ -0,0 +1,73 @@
+// Closures in javascript
+
+function outer() {
+    var name = "Abhishek"
+    function inner(num) {
+        console.log(name, num)
+    }
+    return inner;
+}
+outer()(4);
+
+
+// ques1 =  What will be the output of function
+
+let count = 0;
+(function printCount() {
+    if (count === 0) {
+        let count = 1;
+        console.log(count)
+    }
+
+    console.log(count)
+})();
+
+// ques2 : Write a function that allow you to do this
+
+// Write your Code here
+
+function createBase(a) {
+    return function (b) {
+        console.log(a + b);
+    }
+}
+var addSix = createBase(6);
+addSix(10);
+addSix(21);
+
+// ques 3 Time Optimization
+
+function find() {
+    let a = [];
+    for (let i = 0; i < 100000; i++) {
+        a[i] = i * i;
+    }
+
+    // optimze using closures
+
+    return function (index) {
+        console.log(a[index])
+
+    }
+}
+const clousres = find();
+console.time("6")
+clousres(6)
+console.timeEnd("6")
+console.time("12")
+clousres(12)
+console.timeEnd("12")
+console.time("50")
+clousres(50)
+console.timeEnd("50")
+
+//Block scope and set timeout
+
+for (var i = 0; i < 3; i++) {
+    setTimeout(((i) => {
+        return function () {
+            console.log(i)
+        }
+    })(i), 1000)
+}
+
