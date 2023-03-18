727f7c78c34ee35436aaa122249badd34976e5ed
abhishekkhanna2710 <abhishekkhanna2710@gmail.com>
2023-03-18 22:20:12 +0530

    more js code

--- javascript_projects/index.js
+++ javascript_projects/index.js
@@ -1,48 +1,108 @@
-async function getData() {
-    const url = "https://jsonplaceholder.typicode.com/posts";
+// async function getData() {
+//     const url = "https://jsonplaceholder.typicode.com/posts";
 
-    // fetch(url)
-    //     .then(res => res.json())
-    //     .then(data =>
-    //         // console.log(data[0])
-    //         displayData(data)
-    //     )
-    //     .catch(err => {
-    //         console.log(err)
-    //     })
+//     // fetch(url)
+//     //     .then(res => res.json())
+//     //     .then(data =>
+//     //         // console.log(data[0])
+//     //         displayData(data)
+//     //     )
+//     //     .catch(err => {
+//     //         console.log(err)
+//     //     })
 
 
-    // await async
+//     // await async
 
-    const response = await fetch(url);
+//     const response = await fetch(url);
 
-    const data = await response.json();
-    console.log(data)
-    if (response) {
-        hideloader();
+//     const data = await response.json();
+//     console.log(data)
+//     if (response) {
+//         hideloader();
+//     }
+//     displayData(data);
+
+// }
+
+// getData();
+
+// function hideloader() {
+//     document.getElementById('loading').style.display = 'none'
+// }
+
+
+// function displayData(data) {
+//     let todoData = document.getElementById("todo");
+
+//     let ul = document.createElement('ul');
+
+//     data.forEach(e => {
+//         let li = document.createElement('li');
+//         li.innerHTML = `${e.id} - ${e.title} - ${e.body}`;
+//         ul.appendChild(li);
+
+//     })
+//     todoData.appendChild(ul)
+
+// }
+
+
+// function value(b) {
+//     const a = 100;
+//     a = b;
+//     return a;
+// }
+
+
+// console.log(value(100))
+
+
+let fname = "Masai"
+let lname = "School"
+
+const person = { fname, lname }
+
+console.log(person)
+
+
+// ******************************
+
+const arr = ['albert', 'shnat', "rtex"];
+
+arr[0] = "yogesh"
+
+console.log(arr[0])
+
+// ******************************
+
+function getName() {
+    names = 'albert'
+}
+
+getName();
+console.log(names)
+
+
+var isAuth;
+
+console.log(isAuth)
+
+
+var str = 'Abhishek'
+
+var { length } = str;
+
+
+// *********************************************
+function getname() {
+    var status = false;
+
+    if (status) {
+        const rext = 'masai'
     }
-    displayData(data);
+    console.log(rext)
 
 }
 
-getData();
-
-function hideloader() {
-    document.getElementById('loading').style.display = 'none'
-}
-
-
-function displayData(data) {
-    let todoData = document.getElementById("todo");
-
-    let ul = document.createElement('ul');
-
-    data.forEach(e => {
-        let li = document.createElement('li');
-        li.innerHTML = `${e.id} - ${e.title} - ${e.body}`;
-        ul.appendChild(li);
-
-    })
-    todoData.appendChild(ul)
-
-}
+getname();
