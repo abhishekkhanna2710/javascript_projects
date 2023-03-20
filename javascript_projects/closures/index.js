48a97bc6d99d468f9c7f43a0b069171e6a636d68
abhishekkhanna2710 <abhishekkhanna2710@gmail.com>
2023-03-20 15:54:49 +0530

    Togger function using closures

--- javascript_projects/closures/index.js
+++ javascript_projects/closures/index.js
@@ -164,3 +164,30 @@ let x = 6;
 let y = typeof (x == 6)
 
 console.log(y)
+
+
+// toggler function using closures
+console.log("________________________________________________")
+function toggler() {
+    var arr = arguments;
+    var index = -1;
+    return function () {
+        index = index + 1;
+        if (index >= arr.length) {
+            index = 0;
+        }
+        return arr[index];
+    }
+}
+
+const toggle = toggler(1, 2, 3);
+
+console.log(toggle())
+console.log(toggle())
+console.log(toggle())
+console.log(toggle())
+console.log(toggle())
+console.log(toggle())
+console.log(toggle())
+
+console.log("________________________________________________")
