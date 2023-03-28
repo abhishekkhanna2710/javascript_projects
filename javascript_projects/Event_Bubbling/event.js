2d37bcaf6dca47570ab290e3a3e87704817679c6
abhishekkhanna2710 <abhishekkhanna2710@gmail.com>
2023-03-28 23:16:41 +0530

    Propagation

--- javascript_projects/Event_Bubbling/event.js
+++ javascript_projects/Event_Bubbling/event.js
@@ -1,6 +1,34 @@
 var div = document.querySelector("div");
-
+var button = document.querySelector("button");
 
 div.addEventListener("click", () => {
     console.log("div")
 })
+//************Event bubbling***********
+
+// button.addEventListener("click", () => {
+//     console.log("button")
+// })
+
+//**********Stop prpogation code***********
+
+button.addEventListener("click", (event) => {
+    // event.stopPropagation();
+    console.log("button")
+})
+
+// Immmediate Propagation
+button.addEventListener("click", (event) => {
+    event.stopImmediatePropagation();
+    console.log("button1")
+})
+
+// ************** Event Capturing ***********
+
+// button.addEventListener("click", () => {
+//     console.log("button")
+// }, true)
+
+// ***********Theory***********
+// In event bubbling : it goes from child to parent
+// In event Capturing : it goes from parent to child => just put true 
