14dfda3b4d511cb8eb10bb835aff25d35f055363
abhishekkhanna2710 <abhishekkhanna2710@gmail.com>
2023-05-01 20:49:18 +0530

    2nd question

--- Javscript/index.js
+++ Javscript/index.js
@@ -2,9 +2,21 @@
 
 (function () {
     // b = 3 comes under global scope
-    var a = b // block scope
+    // var a = b // block scope
     var a = b = 3;
 })();
 
 console.log(typeof (a))// undefined 
 console.log(typeof (b))// number
+
+
+// 2nd questions 
+
+function foo(){
+    return
+     {
+        name:"Abhishek"
+    }
+}
+
+console.log(foo())
