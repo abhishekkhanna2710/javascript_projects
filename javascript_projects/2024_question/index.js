24d27b9b5e1824796d77e5d05538885f0c670041
abhishekkhanna2710 <abhishekkhanna2710@gmail.com>
2024-07-14 20:19:21 +0530

    js questions 2

--- javascript_projects/2024_question/index.js
+++ javascript_projects/2024_question/index.js
@@ -1,3 +1,4 @@
+// 1. Array Rotation
 // Write a function to rotate an array to the right by a given number of steps.
 //  For example, if the input array is [1, 2, 3, 4, 5] and the number of steps is 2,
 //   output should be [4, 5, 1, 2, 3].
@@ -22,3 +23,38 @@ function rotateArray(arr, steps) {
 const arr = [1, 2, 3, 4, 5];
 const steps = 2;
 console.log(rotateArray(arr, steps)); // Output: [4, 5, 1, 2, 3]
+
+
+// 2. Remove Duplicates
+// Problem: Write a function to remove duplicate elements from an array and return the new array. For example, if the input array is [1, 2, 2, 3, 4, 4, 5],
+//  the output should be [1, 2, 3, 4, 5].
+
+const array2 = [1, 2, 2, 3, 4, 4, 5];
+ //                   i  
+//                 
+
+let n = array2.length;
+
+
+var i = 0;
+var j = 0;
+
+var result = [];
+
+for(i= 0; i<n;i++){
+    var duplicate = false;
+    for(j = 0; j<result.length;j++){
+        if(array2[i] === result[j]){
+            duplicate = true;
+            break;
+        }
+
+    }
+
+    if(!duplicate){
+        result.push(array2[i]) // 1 2 
+    }
+}
+
+console.log(result)
+
